b327228b43ef47addbdece93f3b2b07676079706
ibywater <isabella.bywater@outlook.com>
2026-05-03 20:46:24 +1000

    Add files via upload

---
+++ 8.plots/F_H3K9ac_narrow.R
@@ -0,0 +1,106 @@
+library(tidyverse)
+
+setwd("~/Uni/2026/BABS3301/Data")
+
+rm(list=ls())
+
+data <- read_tsv("tsv/F_H3K9ac_narrow_auto_data.tsv", skip=2, col_names=FALSE)
+
+########## autosome ################
+
+# extract quartile labels from column 2 and signal values from columns 3 onwards
+plot_data <- data %>%
+  select(-X1) %>%                          # drop sample name column
+  rename(quartile = X2) %>%               # label column 2 as quartile
+  pivot_longer(cols = -quartile,           # pivot all bin columns to long format
+               names_to = "bin",
+               values_to = "signal") %>%
+  mutate(bin = as.numeric(gsub("X", "", bin)) - 3)  # convert bin to position, centered on TSS
+
+# create bp positions (-10000 to +10000)
+n_bins <- 400
+positions <- seq(-10000, 10000, length.out = n_bins)
+
+# add positions
+plot_data <- plot_data %>%
+  group_by(quartile) %>%
+  mutate(position = positions) %>%
+  ungroup()
+
+plot_data <- plot_data %>%
+  mutate(quartile = gsub("F_auto_", "", quartile)) %>%
+  mutate(quartile = gsub(".bed", "", quartile))
+
+
+### mine? ####
+ggplot(plot_data, aes(x = position, y = signal, color = quartile)) +
+  geom_point(size = 0.1, alpha = 0.5, shape = 18) +
+  geom_line() +
+  theme_classic() +
+  # ylim(0.2, 1.1) +
+  # ylim(0.16, 2.82) +
+  geom_vline(xintercept = 0, linetype = "dashed", color = "black", size = 0.5) +
+  scale_color_manual(values = c("Q1" = "blue",
+                                "Q2" = "green",
+                                "Q3" = "orange",
+                                "Q4" = "red")) +
+  geom_smooth(method = "loess", span = 0.25) +
+  labs(x = "Position on Autosomes Relative to TSS (bp)",
+       y = "Normalised Read Depth",
+       color = "Expression quartile",
+       title = "F_H3K9ac (narrow) signal around TSS") +
+  theme(plot.title = element_text(size = 18), axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))
+
+ggsave("actuals_plots/H3K9ac/T2_F_H3K9ac_narrow_Quartiles_10kb_auto.png", dpi=300,
+       width = 5.84, height = 3.95)
+
+######### X Chromosome ##########
+
+data <- read_tsv("tsv/F_H3K9ac_narrow_X_data.tsv", skip=2, col_names=FALSE)
+
+plot_data <- data %>%
+  select(-X1) %>%                          # drop sample name column
+  rename(quartile = X2) %>%               # label column 2 as quartile
+  pivot_longer(cols = -quartile,           # pivot all bin columns to long format
+               names_to = "bin",
+               values_to = "signal") %>%
+  mutate(bin = as.numeric(gsub("X", "", bin)) - 3)  # convert bin to position, centered on TSS
+
+# create bp positions (-10000 to +10000)
+n_bins <- 400
+positions <- seq(-10000, 10000, length.out = n_bins)
+
+# add positions
+plot_data <- plot_data %>%
+  group_by(quartile) %>%
+  mutate(position = positions) %>%
+  ungroup()
+
+plot_data <- plot_data %>%
+  mutate(quartile = gsub("F_X_", "", quartile)) %>%
+  mutate(quartile = gsub(".bed", "", quartile))
+
+
+### mine? ####
+ggplot(plot_data, aes(x = position, y = signal, color = quartile)) +
+  geom_point(size = 0.1, alpha = 0.5, shape = 18) +
+  geom_line() +
+  theme_classic() +
+  # ylim(0.2, 1.1) +
+  # ylim(0.16, 2.82) +
+  geom_vline(xintercept = 0, linetype = "dashed", color = "black", size = 0.5) +
+  scale_color_manual(values = c("Q1" = "blue",
+                                "Q2" = "green",
+                                "Q3" = "orange",
+                                "Q4" = "red")) +
+  geom_smooth(method = "loess", span = 0.25) +
+  labs(x = "Position on X chromosome Relative to TSS (bp)",
+       y = "Normalised Read Depth",
+       color = "Expression quartile",
+       title = "F H3K9ac (narrow) signal around TSS") +
+  theme(plot.title = element_text(size = 18), axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))
+
+ggsave("actuals_plots/H3K9ac/T2_F_H3K9ac_narrow_Quartiles_10kb_ChrX.png", dpi=300,
+       width = 5.84, height = 3.95)
